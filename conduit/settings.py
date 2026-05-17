b29fa3520cbf5239dcb022ca402cd06cc207e7d8
Raul Ciucalau <ciucalauraul@gmail.com>
2026-05-17 11:54:53 +0200

    feat: update CORS_ORIGIN_WHITELIST to use environment variable for better flexibility

--- conduit/settings.py
+++ conduit/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CORS_ORIGIN_WHITELIST = (
-    '0.0.0.0:4000',
-    'localhost:4000',
-)
+CORS_ORIGIN_WHITELIST = os.environ.get(
+    "CORS_ALLOWED_ORIGINS",
+    "localhost:8282,127.0.0.1:8282"
+).split(",")
 
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
